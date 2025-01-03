b96aa6247bc8a2a26a2177dc66f17a63848af787
temzero <nguyentrannhan.00@gmail.com>
2025-01-03 15:11:22 +0700

    update 2

--- frontend/src/components/ClickOutSide/useClickOnlyOutside.js
+++ frontend/src/components/ClickOutSide/useClickOnlyOutside.js
@@ -3,14 +3,14 @@ import { useEffect } from 'react';
 const useClickOnlyOutside = (ref, callback) => {
     useEffect(() => {
         const handleClickOutside = (event) => {
-
-            const isInsidePikaday = event.target.closest('.pika-single');
-            const isPikadayButton = event.target.classList.contains('pika-button');
+            // const isInsidePikaday = event.target.closest('.pika-single');
+            // const isPikadayButton = event.target.classList.contains('pika-button');
             if (
                 ref.current &&
-                !ref.current.contains(event.target) && // Not inside the form
-                !isInsidePikaday && // Not inside Pikaday popup
-                !isPikadayButton // Not clicking a date button
+                !ref.current.contains(event.target)
+                // && // Not inside the form
+                // !isInsidePikaday && // Not inside Pikaday popup
+                // !isPikadayButton // Not clicking a date button
             ) {
                 callback(); 
             }
